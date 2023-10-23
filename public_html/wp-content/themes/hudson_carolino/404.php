b31d665ea5ebe4e8e44309f859fcf0211f1c88de
Hudson Carolino <carolinohudson@gmail.com>
2023-10-23 22:54:49 +0200

    Update 404.php

--- public_html/wp-content/themes/hudson_carolino/404.php
+++ public_html/wp-content/themes/hudson_carolino/404.php
@@ -20,6 +20,7 @@ get_header(); ?>
 
 
 <div style="height:1000px;background:#555dff;position:relative">
+	<div hidden>Div</div>
 	<img src="<?php bloginfo('template_directory'); ?>/img/logo2.svg" alt="404" style="max-width: 700px; position: absolute;top:50%;left:50%;transform: translate(-50%, -50%);">
 </div>
 
